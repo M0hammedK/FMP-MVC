c6af4a0ee07a0b3384ee9f5d6d70169e173dd742
M0hammed <mmm2016alkaf@gmail.com>
2024-10-18 00:32:07 +0300

    Add project files.

---
+++ FMP-MVC/Models/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FMP_MVC.Models
+{
+    public class Ticket
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        [ForeignKey("Movie")]
+        public int Movie_Id { get; set; }
+        public virtual Movie Movie { get; set; }
+        [Required]
+        [ForeignKey("Customer")]
+        public int Customer_Id { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
